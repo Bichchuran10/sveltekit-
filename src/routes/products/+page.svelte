7d572612e2186f8acede1dd80a1ea2ee0e306331
Bichchuran10 <bichchuran10@gmail.com>
2023-05-08 17:43:39 +0530

    route navigation

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,4 +1,10 @@
+<script>
+        export let productId=100;
+</script>
+
+<a href="/">Home</a>
+
 <h1>Product List</h1>
-<h2> Product 1 </h2>
-<h2> Product 2 </h2>
-<h2> Product 3 </h2>
+<h2> <a href="/products/1"> Product 1 </h2>
+<h2> <a href="/products/2"> Product 2 </h2>
+<h2> <a href="{`/products/${productId}`}> Product 3 </h2>
