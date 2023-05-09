8714016718aa287df9a880875fd95b4381d26c15
Bichchuran10 <bichchuran10@gmail.com>
2023-05-09 12:08:36 +0530

    cleaner

---
+++ src/lib/footer.svelte
@@ -0,0 +1,13 @@
+<footer class="layout-footer">Footer</footer>
+
+<style>
+
+    .layout-footer{
+        background-color: #48494B;
+        color:#808588;
+        font-size:30px;
+        text-align:center;
+        padding:10px
+    }
+
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,10 @@
-<header class="layout-header">Header</header>
-<slot/>
-<footer class="layout-footer">Footer</footer>
+<script>
+    import Header from '$lib/header.svelte'
+    import Footer from '$lib/footer.svelte'
+</script>
 
-<style>
-    .layout-header{
-        background-color: #787276;
-        color:#808588;
-        font-size:30px;
-        text-align:center;
-        padding:10px
-    }
-        .layout-footer{
-        background-color: #48494B;
-        color:#808588;
-        font-size:30px;
-        text-align:center;
-        padding:10px
-    }
-</style>
+<Header/>
+<slot/>
+<Footer/>
+
+
