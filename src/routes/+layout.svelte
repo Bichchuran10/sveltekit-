93bc3d60c7aac3856d3a2f0ae73ad3eafe12b8ad
Bichchuran10 <bichchuran10@gmail.com>
2023-05-09 11:21:13 +0530

    route layout slotttt

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<header class="layout-header">Header</header>
+<slot/>
+<footer class="layout-footer">Footer</footer>
+
+<style>
+    .layout-header{
+        background-color: blue;
+        color:orange;
+        font-size:30px;
+        text-align:center;
+        padding:10px
+    }
+        .layout-footer{
+        background-color: blue;
+        color:orange;
+        font-size:30px;
+        text-align:center;
+        padding:10px
+    }
+</style>
