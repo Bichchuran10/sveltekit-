fb16b3ff060cf943fa491e970b66b9249cd92160
Bichchuran10 <bichchuran10@gmail.com>
2023-05-09 10:48:07 +0530

    navigating programatically

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -5,6 +5,9 @@
 <a href="/">Home</a>
 
 <h1>Product List</h1>
-<h2> <a href="/products/1"> Product 1 </h2>
-<h2> <a href="/products/2"> Product 2 </h2>
-<h2> <a href="{`/products/${productId}`}> Product 3 </h2>
+<h2> <a href="/products/1"> Product 1 </a> </h2>
+<h2> <a href="/products/2">Product 2 </a></h2>
+<h2> <a href="/products/3"> Product 3 </a> </h2>
+<h2> <a href={`products/${productId}`}>Product {productId} </a> </h2>
+
+
